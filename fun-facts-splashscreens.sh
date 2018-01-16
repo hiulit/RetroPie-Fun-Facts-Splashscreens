39b8356b3ad7232c5e41e43ec7e0e5bc6d18d9fd
hiulit <hiulit@gmail.com>
2018-01-16 16:33:08 +0100

    Fixed update function

--- fun-facts-splashscreens.sh
+++ fun-facts-splashscreens.sh
@@ -355,8 +355,8 @@ function check_updates() {
     git remote update > /dev/null
     UPSTREAM="$1@{u}"
     LOCAL="$(git rev-parse @)"
-    REMOTE="$(git rev-parse $UPSTREAM > /dev/null)"
-    BASE="$(git merge-base @ $UPSTREAM > /dev/null)"
+    REMOTE="$(git rev-parse $UPSTREAM)"
+    BASE="$(git merge-base @ $UPSTREAM)"
     if [[ "$LOCAL" == "$REMOTE" ]]; then
         updates_status="up-to-date"
         updates_output="up to date"
