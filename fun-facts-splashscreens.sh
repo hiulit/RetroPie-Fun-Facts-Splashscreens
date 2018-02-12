616f479ea56779ffb084c5436179bcbca2800fb1
hiulit <hiulit@gmail.com>
2018-02-12 09:21:04 +0100

    Bumped to v2.0.0

--- fun-facts-splashscreens.sh
+++ fun-facts-splashscreens.sh
@@ -27,7 +27,7 @@ readonly SPLASH_LIST="/etc/splashscreen.list"
 readonly RCLOCAL="/etc/rc.local"
 readonly DEPENDENCIES=("imagemagick")
 
-readonly SCRIPT_VERSION="1.5.0"
+readonly SCRIPT_VERSION="2.0.0"
 readonly SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 readonly SCRIPT_NAME="$(basename "$0")"
 readonly SCRIPT_FULL="$SCRIPT_DIR/$SCRIPT_NAME"
