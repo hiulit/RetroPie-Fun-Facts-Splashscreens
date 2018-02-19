416de0b2a52c1e53396bb9c6cecfa8a7bf881581
hiulit <hiulit@gmail.com>
2018-02-19 17:29:40 +0100

    Added rom path

--- fun-facts-splashscreens-runcommand-onend.sh
+++ fun-facts-splashscreens-runcommand-onend.sh
@@ -4,7 +4,8 @@
 readonly RP_CONFIG_DIR="/opt/retropie/configs"
 readonly SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 readonly SYSTEM="$1"
+readonly ROM_PATH="$3"
 
 [[ -f "$RP_CONFIG_DIR/$SYSTEM/launching.png"  ]] && rm "$RP_CONFIG_DIR/$SYSTEM/launching.png"
 
-"$SCRIPT_DIR/fun-facts-splashscreens.sh" --create-fun-fact "$SYSTEM" #> /dev/null
+"$SCRIPT_DIR/fun-facts-splashscreens.sh" --create-fun-fact "$SYSTEM" "$ROM_PATH" #> /dev/null
