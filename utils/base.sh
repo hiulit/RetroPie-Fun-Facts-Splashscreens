249f84760e06d58250bd4f8ed832a86c58fc7d54
hiulit <hiulit@gmail.com>
2018-04-03 21:01:22 +0200

    Added error_report to check_arguments

--- utils/base.sh
+++ utils/base.sh
@@ -46,6 +46,11 @@ function check_dependencies() {
 
 function check_argument() {
     # Note: this method doesn't accept arguments starting with '-'.
+    if [[ -z "$1" ]]; then
+        error_report
+        log "Missing arguments to check."
+        exit 1
+    fi
     if [[ -z "$2" || "$2" =~ ^- ]]; then
         log "ERROR: '$1' is missing an argument."
         echo "Try 'sudo $0 --help' for more info." >&2
@@ -104,11 +109,11 @@ function log() {
     check_log_file
     if [[ "$GUI_FLAG" -eq 1 ]] ; then
         #~ echo "$(date +%F\ %T) - (v$SCRIPT_VERSION) GUI: $* << ${FUNCNAME[@]:1:((${#FUNCNAME[@]}-3))} $OPTION" >> "$LOG_FILE" # -2 are log ... get_options main main
-        echo "$(date +%F\ %T) - (v$SCRIPT_VERSION) GUI: $*" >> "$LOG_FILE"
+        echo "$(date +%F\ %T) - (v$SCRIPT_VERSION) GUI: $* << $OPTION" >> "$LOG_FILE"
         echo "$*"
     else
         #~ echo "$(date +%F\ %T) - (v$SCRIPT_VERSION) $* << ${FUNCNAME[@]:1:((${#FUNCNAME[@]}-3))} $OPTION" >> "$LOG_FILE" # -2 are log ... get_options main main
-        echo "$(date +%F\ %T) - (v$SCRIPT_VERSION) $*" >> "$LOG_FILE"
+        echo "$(date +%F\ %T) - (v$SCRIPT_VERSION) $* << $OPTION" >> "$LOG_FILE"
         echo "$*" >&2
     fi
 }
